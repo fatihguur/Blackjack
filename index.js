4c30eb723044763eb96d97087f252b8cf7dced57
Fatih Gur <fatih.gur26@hotmail.com>
2022-11-03 15:51:54 +0300

    Version 0.0.1

---
+++ index.js
@@ -0,0 +1,24 @@
+let firstCard = 6;
+let secondCard = 9;
+let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+
+
+function startGame() {
+    sumEl.textContent = "Sum: " + sum;
+    if (sum < 21) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!";
+        hasBlackjack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
+}
